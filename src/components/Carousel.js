b7d11d177efa8e365d627cec7bc37d37ab2c2484
unknown <hashmiabdulmajid@gmail.com>
2024-08-02 15:40:13 +0530

    testing

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,10 +2,11 @@ import React from "react";
 import Slider from "react-slick";
 import './Main.css';
 import './Carousel.css';
-import page1 from "./../images/page1.png";
-import page2 from "./../images/page2.png";
-import page3 from "./../images/page3.png";
-import page4 from "./../images/page4.png";
+import { Link } from "react-router-dom";
+// import page1 from "./../images/page1.png";
+// import page2 from "./../images/page2.png";
+// import page3 from "./../images/page3.png";
+// import page4 from "./../images/page4.png";
 
 
 
@@ -28,17 +29,23 @@ function SimpleSlider() {
     <div className="slider-container">
       <Slider {...settings}>
         <div>
-          <img src={page1} loading='lazy' className="images" />
+          
+          <img src='https://i.ibb.co/LzXj6CT/Untitled-4.webp' loading='lazy' className="images" />
         </div>
         <div>
-          <img src={page2} loading='lazy' className="images" />
+        <Link to='/product-detail/66a7657e27ab209cfdfa1423'>
+          <img src='https://i.ibb.co/P6PyYLD/Black-Simple-Special-Ramadan-Dates-Instagram-Post.webp' loading='lazy' className="images" />
+          </Link>      
         </div>
+        
         <div>
-          <img src={page3} loading='lazy' className="images" />
-        </div>
-        <div>
-          <img src={page4} loading='lazy' className="images" />
-        </div>
+          <Link to='product-detail/66a7676a27ab209cfdfa1432'>
+          <img src='https://i.ibb.co/6NwRDRD/Ramadan-Dates-For-Iftar-Instagram-Story-3000-x-1055-px.webp' loading='lazy' className="images" />
+          </Link></div>
+          <div>
+          <Link to=''>
+          <img src='https://i.ibb.co/xDBhvFk/Yellow-Modern-Dates-Benefits-Facebook-Post.webp' loading='lazy' className="images" />
+          </Link></div>
         
         
         
